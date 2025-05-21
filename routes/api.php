624507220dad7710316f8155433c967787a1687d
afifao1 <afifao1>
2025-05-21 18:37:47 +0500

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TransactionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource('transactions', TransactionController::class);
 Route::get('/balance', [TransactionController::class, 'balance']);
